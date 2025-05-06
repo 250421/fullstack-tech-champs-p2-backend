b7d666fd09579f2fbd7ae631853c65b982e078ab
Shanjhitha <Shanjhitha.Kannan.2003@gmail.com>
2025-05-06 16:58:14 -0400

    Initial commit on user branch SK

---
+++ nflfantasydraft/src/main/java/com/revature/nflfantasydraft/NflFantasyDraftGameApplication.java
@@ -0,0 +1,13 @@
+package com.revature.nflfantasydraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NflFantasyDraftGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NflFantasyDraftGameApplication.class, args);
+	}
+
+}
