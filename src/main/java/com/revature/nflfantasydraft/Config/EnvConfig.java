d1d1f16d301090fb9be6f26e4760adfd064f3235
MarkBahr <bahrmark147@gmail.com>
2025-05-17 21:48:27 -0500

    Edited envConfig to access env vars from OS/Jenkins directly

--- src/main/java/com/revature/nflfantasydraft/Config/EnvConfig.java
+++ src/main/java/com/revature/nflfantasydraft/Config/EnvConfig.java
@@ -1,15 +1,25 @@
 package com.revature.nflfantasydraft.Config;
-import io.github.cdimascio.dotenv.Dotenv;
+// import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Configuration;
+
+// uncomment next line for local developemnt
+// import io.github.cdimascio.dotenv.Dotenv;
 
 
 @Configuration
 public class EnvConfig {
     public EnvConfig() {
-        Dotenv dotenv = Dotenv.configure().load();
-        dotenv.entries().forEach(entry -> {
-            System.setProperty(entry.getKey(), entry.getValue());
-        });
+        // Access the environment variables from the OS/Jenkins directly
+        String apiKey = System.getenv("YOUR_API_KEY");
+        String otherKey = System.getenv("YOUR_OTHER_KEY");
+
+        if (apiKey != null) System.setProperty("YOUR_API_KEY", apiKey);
+        if (otherKey != null) System.setProperty("YOUR_OTHER_KEY", otherKey);
+
+        // For local development only. Uncomment below for running on your machine
+        // Dotenv dotenv = Dotenv.configure().load();
+        // dotenv.entries().forEach(entry -> {
+        //     System.setProperty(entry.getKey(), entry.getValue());
+        // });
     }
 }
-  
